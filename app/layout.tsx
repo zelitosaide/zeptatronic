e930357d70030d4f694f213f7123e1f63403d759
Zelito A. Saide <zelitosaide83@gmail.com>
2025-01-05 21:24:22 +0200

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "ZETA",
+  title: "ZEPTA",
   description: "All components you need for your project!",
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,20 @@ export default function Home() {
   // console.log({ users });
 
   return (
-    <h1>Zeptatronic</h1>
+    <>
+      <h1>Zeptatronic</h1>
+      <div>
+        <h1 className="text-red-300">Routing Files</h1>
+        <p>layout ✅</p>
+        <p>page ✅</p>
+        <p>loading: Loading UI</p>
+        <p>not-found: Not found UI</p>
+        <p>error: Error UI</p>
+        <p>global-error: Global error UI</p>
+        <p>route: API endpoint</p>
+        <p>template: Re-rendered layout</p>
+        <p>default: Parallel route fallback page</p>
+      </div>
+    </>
   );
 }
