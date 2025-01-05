4b001ee25a4dc1562a02e90a1fd789cc1cf7b70f
Zelito A. Saide <zelitosaide83@gmail.com>
2025-01-05 20:24:38 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
+// import { prisma } from "./db/prisma";
 
-export default function Home() {
+export default async function Home() {
+  // const users = await prisma.account.findMany();
+  // console.log({ users });
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -14,7 +18,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            ..Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
