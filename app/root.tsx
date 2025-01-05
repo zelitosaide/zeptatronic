cab81e6d0fc785ac76b21090376f8daa8b61bedd
Zelito A. Saide <zelitosaide83@gmail.com>
2025-01-05 18:20:05 +0200

    .

--- app/root.tsx
+++ app/root.tsx
@@ -47,7 +47,7 @@ export default function App() {
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = "Oops!";
+  let message = "Oops!!";
   let details = "An unexpected error occurred.";
   let stack: string | undefined;
 
