8d06d76c67ee2b561d76bba98b547fa507985bab
Zelito A. Saide <zelitosaide83@gmail.com>
2025-01-07 00:13:38 +0200

    .

--- app/comps/page.tsx
+++ app/comps/page.tsx
@@ -8,14 +8,14 @@ export default async function Page() {
   return (
     <>
       <form action={createComp}>
-        {/* <button disabled type="submit">Create Comp (disabled)</button> */}
-        <button type="submit">Create Comp</button>
+        <button disabled type="submit">Create Comp (disabled)</button>
+        {/* <button type="submit">Create Comp</button> */}
       </form>
 
       <h1>Comps</h1>
       <ul>
         {comps.map((comp) => (
-          <Comp key={comp} name={comp} />
+          <Comp key={comp.id} name={comp.name} />
         ))}
       </ul>
     </>
--- lib/comps.ts
+++ lib/comps.ts
@@ -1,5 +1,15 @@
-export function getComps(): Promise<string[]> {
-  return new Promise((resolve) => {
-    resolve(["Diodo", "Transistor"]);
+// export function getComps(): Promise<string[]> {
+//   return new Promise((resolve) => {
+//     resolve(["Diodo", "Transistor"]);
+//   });
+// }
+
+import { prisma } from "@/db/prisma";
+
+export async function getComps() {
+  return await prisma.comp.findMany({
+    where: {
+      isActive: true
+    }
   });
 }
